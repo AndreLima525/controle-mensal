f7ca53f5eca39d85b4dfefa669bc0cb60cc38615
Andre <andre525luis@gmail.com>
2026-02-20 11:13:27 -0300

    Primeira versão do sistema de controle de gastos

---
+++ view/index.php
@@ -0,0 +1,38 @@
+<?php
+	require_once('../controller/fnLoginController.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Página Inicial</title>
+	<link rel="stylesheet" type="text/css" href="../styles/styleIndex.css">
+</head>
+<body>
+	<div class="container"> 
+		
+			<div class="card">
+
+				<h1>Controle de Gastos</h1>
+
+				<form method="POST">
+						<label> Usuário </label>
+						<input type="text" name="usuario" required>
+
+						<label> Senha </label>
+						<input type="password" name="senha" required>
+
+						<button> Entrar </button>
+
+						<hr>
+						<a href="" class="forgot"> Esqueceu sua senha? </a>
+					</form>
+
+				</div>
+
+			
+		</body>
+		</html>

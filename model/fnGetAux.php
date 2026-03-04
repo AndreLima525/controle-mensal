5e3e6ce1d37f95429cc703cebeba3c3b7d14e1f4
Andre <andre525luis@gmail.com>
2026-03-04 15:05:41 -0300

    ok

--- model/fnGetAux.php
+++ model/fnGetAux.php
@@ -1,8 +1,10 @@
 <?php
     
-    require_once('conn.php');
+require_once('conn.php');
 
-	function buscarCategorias() {
+$usuarioLogin = $_SESSION['idUsuario'];
+
+function buscarCategorias() {
     global $pdo;
 
     $sqlCategorias = "SELECT * FROM categorias";
@@ -17,6 +19,17 @@ function buscarPrioridades() {
 
     $sqlPrioridades = "SELECT * FROM prioridadesDespesas";
     $stmt = $pdo->prepare($sqlPrioridades);
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function buscarPix($usuarioLogin) {
+    global $pdo;
+
+    $sqlPix = "SELECT * FROM dadospix WHERE idUsuario = :usuario";
+    $params = [':usuario' => $usuarioLogin];
+    $stmt = $pdo->prepare($sqlPix);
     $stmt->execute();
 
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
--- view/modalCaixinha.php
+++ view/modalCaixinha.php
@@ -1,11 +1,53 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-</head>
-<body>
-	caixinha
-</body>
-</html>
+<?php 
+
+include_once('../model/fnGetAux.php');
+include_once('../controller/fnIncluirCaixinhaController.php');
+
+?>
+
+
+<link rel="stylesheet" type="text/css" href="../styles/stylePages.css">
+<div class="page-header">
+	<h2>
+		Incluir Novo Depósito
+	</h2>
+</div>
+
+<div class="card">
+
+	
+	<form class="form-grid" method="POST">
+
+    <div class="form-group">
+        <label>Data</label>
+        <input type="date" name="data" required>
+    </div>
+
+    <div class="form-group">
+        <label>Valor - R$</label>
+        <input type="text" name="valorDespesa" required>
+    </div>
+
+    <div class="form-group">
+        <label>Chave Pix</label>
+        <input type="text" name="" disabled value="">
+    </div>
+
+    <div class="form-group full-width">
+        <label>Descrição</label>
+        <textarea name="dsDespesa" rows="4" placeholder="Digite a descrição..." required></textarea>
+    </div>
+
+    <div class="form-actions center">
+        <a href="../view/dashboard.php" class="btn btn-voltar" name="voltar">
+            <i class="fa-solid fa-arrow-left"></i> Cancelar
+        </a>
+
+        <button type="submit" class="btn btn-primary" name="novo">
+            <i class="fa-solid fa-plus-circle"></i> Incluir Despesa
+        </button>
+    </div>
+
+</form>
+
+</div>

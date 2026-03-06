3d7eaa9f48bb5b0d2a8990c572f3dbe7b027c7a8
Andre <andre525luis@gmail.com>
2026-03-06 15:49:42 -0300

    OK

--- controller/fnConfigController.php
+++ controller/fnConfigController.php
@@ -17,4 +17,38 @@
 	
 	$dadosUsuarios = buscarUsuariosFiltrados( $nomeUsuario, $stAtivo);
 }
+
+if (isset($_POST['novo'])) {
+	
+	$dados = [
+
+		'nomeUsuario'  => $_POST['nomeUsuario'],
+		'login'      => $_POST['login'],
+		'senha'  => $_POST['senha'],		
+		'idNivelAcesso'=> $_POST['idNivelAcesso']
+	];
+
+	$dadosPix = [
+
+		'dsBancoPix' => $_POST['dsBancoPix'],
+		'dsPix' => $_POST['dsPix']
+	];
+
+	$inseriu = inserirUsuario($pdo, $dados, $dadosPix);
+
+	if ($inseriu) {
+
+			echo "
+
+			<META HTTP-EQUIV=REFRESH CONTENT = '0;URL='>
+			<script type=\"text/javascript\">
+			alert(\"Usuário cadastrado com sucesso!\");
+			</script>                            
+			";
+
+		} else {
+			echo "<script>alert('Erro ao cadastrar usuário!');</script>";
+		}
+
+}
 ?>
--- model/fnConfigModel.php
+++ model/fnConfigModel.php
@@ -39,4 +39,42 @@ function buscarUsuariosFiltrados($nomeUsuario = null, $stAtivo = null) {
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function inserirUsuario($pdo, $dados, $dadosPix) {
+
+    $sql = "INSERT INTO usuarios (
+                nomeUsuario,
+                login,
+                senha,
+                idNivelAcesso
+            ) VALUES (
+                :nomeUsuario,
+                :login,
+                :senha,
+                :idNivelAcesso
+            )";
+
+    $stmt = $pdo->prepare($sql);
+
+    return $stmt->execute([
+        ':nomeUsuario'    => $dados['nomeUsuario'],
+        ':login'  => $dados['login'],
+        ':senha' => $dados['senha'],
+        ':idNivelAcesso'  => $dados['idNivelAcesso']
+    ]);
+
+    $idUsuario = $pdo->lastInsertId();
+
+     $sqlPix = "INSERT INTO dadospix VALUES (:dsBancoPix, :dsPix, :idUsuario)";
+
+      $stmtPix = $pdo->prepare($sqlPix);
+
+        $stmtPix->execute([
+
+            'dsBancoPix' => $dadosPix['dsBancoPix'],
+            ':dsPix'     => $dadosPix['dsPix'],
+            ':idUsuario' => $idUsuario
+            
+        ]);
+}   
+
 ?>
